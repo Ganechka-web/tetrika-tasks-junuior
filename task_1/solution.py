d226cb1360c637ec2b7768b0a3cf2f3b342eb598
Ganechka <maksim15848@gmail.com>
2025-06-17 13:06:12 +0300

    fix(task_1): add kwargs type checking

--- task_1/solution.py
+++ task_1/solution.py
@@ -4,10 +4,16 @@ from functools import wraps
 
 def strict(func: Callable) -> Callable:
     @wraps(func)
-    def wrapper(*args: Any) -> int:
+    def wrapper(*args: Any, **kwargs: Any) -> int:
+        # check args annotations
         for type, arg in zip(func.__annotations__.values(), args):
             if not isinstance(arg, type):
                 raise TypeError
+        
+        # check kwargs annotations
+        for kwarg, value in kwargs.items():
+            if not isinstance(value, func.__annotations__[kwarg]):
+                raise TypeError 
             
         return func(*args)
     return wrapper
@@ -21,7 +27,12 @@ def add(a: int, b: int) -> int:
 def something(a: list, b: dict, c: str) -> int:
     return len(a) + len(b) + len(c)
 
+@ strict
+def something_with_kwargs(a: int, g: None | int = None, c: set | dict = {}):
+    return (a + g + len(c)) if g and c else a
+
 
 if __name__ == '__main__':
     print(add(2, 3))
     print(something([], {}, ''))
+    print(something_with_kwargs(1, g=2, c={3, 4}))
