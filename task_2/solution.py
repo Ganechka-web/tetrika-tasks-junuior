c929512689f577ea0d0faa98e775cae08c0cccc4
Ganechka <maksim15848@gmail.com>
2025-06-17 11:55:20 +0300

    refactor(task_2): improve writing to the file, comments

--- task_2/solution.py
+++ task_2/solution.py
@@ -1,12 +1,12 @@
 import csv
 from urllib import parse
-from pprint import pprint
 
 import requests 
 from bs4 import BeautifulSoup, Tag
 
 
-def find_next_page_link(html: str) -> str:
+def get_next_page_link(html: str) -> str:
+    """Rerurns found the next page link with protocol and host"""
     pages_toolbar_id = 'mw-pages'
 
     parser = BeautifulSoup(html, 'html.parser')
@@ -21,9 +21,11 @@ def find_next_page_link(html: str) -> str:
 
 
 def count_creatures_on_page(html: str) -> dict[str, int]:
+    """Ruturs counted creatures on the html page"""
     creatures: dict[str, int] = {}
 
     parser = BeautifulSoup(html, 'html.parser')
+    # get all links with creatures titles
     creatures_tags: list[Tag] = (
         parser.find('div', attrs={'id': 'mw-pages'}) 
         .find('div', class_='mw-category mw-category-columns')
@@ -45,16 +47,18 @@ def merge_creatures_dicts(
 
 
 def get_creatures() -> dict[str, int]:
+    """Returns all counted beasts on ru.wikipedia.org"""
     next_page_link = 'https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту'
     creatures_by_alphabet: dict[str, int] = {}
 
+    # Untill first latin letter on the page (russian alphabet is over)
     while 'A' not in creatures_by_alphabet:
         response = requests.get(next_page_link)
         creatures_by_alphabet = merge_creatures_dicts(
             creatures_by_alphabet, 
             count_creatures_on_page(response.text)
         )
-        next_page_link = find_next_page_link(response.text)
+        next_page_link = get_next_page_link(response.text)
 
     del creatures_by_alphabet['A'], creatures_by_alphabet['R']
 
@@ -62,14 +66,19 @@ def get_creatures() -> dict[str, int]:
 
 
 def fill_csv_counted_beasts():
+    """Fill beasts.csv file sorted and counted beasts"""
+    creatures: dict[str, int] = get_creatures()
+    creatures_on_write: tuple[str, int] = sorted(
+        creatures.items(), key=lambda i: i[0].lower()
+    ) 
+
     with open('beasts.csv', 'w', encoding='utf-8', newline='') as file:
         csv_writter = csv.writer(file)
         # write headers
         csv_writter.writerow(['letter', 'count'])
 
-        creatures: dict[str, int] = get_creatures()
-        for item in sorted(creatures.items(), key=lambda i: i[0].lower()):
-            csv_writter.writerow(item)
+        # write all creatures rows
+        csv_writter.writerows(creatures_on_write)
 
 
 if __name__ == '__main__':
